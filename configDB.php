98e2c2cfe1945cf7f5879eb0607071ad52c6ad45
moises <moises_andrades-fil1@estudante.sc.senai.br>
2019-08-23 15:08:56 -0300

    Conexão com o Banco de Dados

---
+++ configDB.php
@@ -0,0 +1,18 @@
+<?php
+//config.php
+
+//Dados para escolher do DataBase (DB)
+$dbhost = "localhost";
+$dbuser = "root"; // Usuário raíz (Rute)
+$dbpass = "";
+$dbname = "sistemaDeLogin";
+
+
+//cpnexão com o banco de dados
+$conecta = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+if ($conecta->connect_error){
+    die("Não foi possível conectar ao Banco de Dados: " . $conecta->connect_error);
+}else {
+        echo"<h1>conectou no BD Manoowwwww! 42  </h1>";
+    
+};
