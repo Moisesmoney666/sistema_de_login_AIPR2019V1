46e60e3f92fb7e7ea7ee27fc98a663a69edb8e77
Moisesmoney666 <49690927+Moisesmoney666@users.noreply.github.com>
2019-08-23 15:11:14 -0300

    Update configDB.php

--- configDB.php
+++ configDB.php
@@ -13,6 +13,6 @@ $conecta = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 if ($conecta->connect_error){
     die("Não foi possível conectar ao Banco de Dados: " . $conecta->connect_error);
 }else {
-        echo"<h1>conectou no BD Manoowwwww! 42  </h1>";
+        //echo"<h1>conectou no BD Manoowwwww! 42  </h1>";
     
 };
