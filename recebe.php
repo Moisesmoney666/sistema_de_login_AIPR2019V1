860fa29afabccfcc3cac873d03ef203f737e0804
Moises <moises_andrades-fil1@estudante.sc.senai.br>
2019-09-19 15:04:52 -0300

    Geraçao de token
    1° passo para criar uma nova senha

--- recebe.php
+++ recebe.php
@@ -12,8 +12,30 @@ function verificar_entrada($entrada)
     $saida = htmlspecialchars($saida);
     return $saida;
 }
+if(isset($_POST['action'])&&
+$_POST['action']='senha'){
+    //Apenas para Debug
+//echo"<strong>Recuperaçao De senha</strong>";//
+$emailSenha = verificar_entrada($_POST['emailSenha']);
+$sql = $conecta->prepare("SELECT idUsuario FROM usuario  WHERE email  =  ?");
+$sql->bind_param("s",$emailSenha);
+$sql->execute();
+$resultado = $sql->get_result();
+if($resultado->num_rows > 0 ){
+        //Existe o usuário no Banco De Dados
+        //Só para testar / debug
+        //echo "<p class=\"text-success\">E-mail encotrado</p>";//
+        $frase = "FreeFIRE@#Èj!oGoD@H#omem*";
+        $frases_secreta = str_shuffle($frase);
+        $token = substr($frases_secreta, 0,10);
+        echo"<p> $token </p> ";
+}else{
+    echo'<p class="text-danger">E-mail não encotrado</p>';
+} 
 
-if (
+
+
+}else if (
     isset($_POST['action']) &&
     $_POST['action'] == 'login'
 ) {
